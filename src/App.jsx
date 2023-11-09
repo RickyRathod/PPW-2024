9b98e01c24a7b6d345e5d63e906e5d734b765765
Rick_ <123428369+RickyRathod@users.noreply.github.com>
2023-11-08 21:58:42 -0500

    Test

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { BrowserRouter } from 'react-router-dom';
+
+//causes 354  build error with - Could not resolve "three"
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components';
+
+const App = () => {
+  return (
+    <div> Portfolio </div>
+  )
+}
+
+export default App
